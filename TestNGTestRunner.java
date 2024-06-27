31cccfbea768a8add3a5e2e9e7938ff4efc8333b
Divya_Sonawane <divyadpks15@gmail.com>
2024-06-27 18:07:16 +0530

    Delete TestNGTestRunner.java

--- TestNGTestRunner.java
+++
@@ -1,9 +0,0 @@
-package cucumberoptions;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features",glue="stepdefinations",monochrome=true)
-public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
-}
